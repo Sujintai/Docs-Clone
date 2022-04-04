e3dbcd3f35841dfbe574d0f4d157301666ce0ebe
JJCai <jcai8242@bths.edu>
2022-04-04 03:47:18 -0400

    client half works, servers work i think

--- session-server/routes/app-router.js
+++ session-server/routes/app-router.js
@@ -4,6 +4,6 @@ const router = express.Router()
 
 router.get('/connect/:id', AppController.connect)
 router.post('/op/:id', AppController.op)
-router.get('/doc/:id', AppController.doc)
+router.get('/doc/:id', AppController.getDoc)
 
 module.exports = router
