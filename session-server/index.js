e254bcb7b9893a5f3c41adf48b4fc652300b0269
justin <justin.cai@stonybrook.edu>
2022-05-01 06:03:07 +0000

    turn sharedb server ip into env

--- session-server/index.js
+++ session-server/index.js
@@ -54,14 +54,14 @@ app.use(cookieParser())
 // SETUP OT SOCKETS
 // Open WebSocket connection to ShareDB server
 //var socket = new ReconnectingWebSocket('ws://' + window.location.host);
-socket = new WebSocket('ws://209.151.151.54:8080');
+socket = new WebSocket(process.env.SHAREDB_SERVER);
 //connection = new sharedb.Connection(socket);
 
 // SETUP SESSION MANAGER
 sessionManager = [];
 let hash = 0; // TODO: Change 0 into hash function for docid
 sessionManager[hash] = { 
-  URL: 'ws://209.151.151.54:8080',
+  URL: process.env.SHAREDB_SERVER,
   socket,
   activeDocuments: []
 }
